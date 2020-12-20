5e7c297b350f86ea91164fdd632938dd8568c6c7
punmy <panmingyang2009@live.cn>
2020-12-20 18:22:40 +0800

    【Feature】Add PixelBinarizationFilter

---
+++ MYImageDiffDemo/MYImageDiffDemo/MYPixelBinarizationFilter.h
@@ -0,0 +1,27 @@
+//
+//  PixelBinarizationFilter.h
+//  WatermarkRemove
+//
+//  Created by 潘名扬 on 2020/2/28.
+//  Copyright © 2020 Punmy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreImage/CoreImage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PixelBinarizationFilter : CIFilter
+
+/// 待对比图像A
+@property (nonatomic, strong, nullable) CIImage *imageA;
+/// 待对比图像B
+@property (nonatomic, strong, nullable) CIImage *imageB;
+
+/// 容差值
+/// @discussion 当某个颜色分量的差异超过容差值，则认为该颜色分量不同
+@property (nonatomic, assign) NSUInteger tolerance;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MYImageDiffDemo/MYImageDiffDemo/MYPixelBinarizationFilter.m
@@ -0,0 +1,66 @@
+//
+//  PixelBinarizationFilter.m
+//  MYImageDiff
+//
+//  Created by 潘名扬 on 2020/2/28.
+//  Copyright © 2020 Punmy. All rights reserved.
+//
+
+#import "PixelBinarizationFilter.h"
+#import <CoreImage/CoreImage.h>
+
+@implementation PixelBinarizationFilter
+
+static CIKernel *customKernel = nil;
+
+- (instancetype)init {
+    self = [super init];
+    if (self &&
+        customKernel == nil) {
+        
+        NSBundle *bundle = [NSBundle bundleForClass: [self class]];
+        NSURL *kernelURL = [bundle URLForResource:@"PixelBinarization"
+                                    withExtension:@"cikernel"];
+        
+        NSError *error;
+        NSString *kernelCode = [NSString stringWithContentsOfURL:kernelURL
+                                                        encoding:NSUTF8StringEncoding
+                                                           error:&error];
+        if (kernelCode == nil) {
+            NSLog(@"Error loading kernel code string in %@\n%@",
+                  NSStringFromSelector(_cmd),
+                  [error localizedDescription]);
+            abort();
+        }
+        
+#if DEBUG
+        NSMutableArray *messageLog = [NSMutableArray array];
+        SEL logSelector = @selector(kernelsWithString:messageLog:);
+        NSArray *kernels = [[CIKernel class] performSelector:logSelector
+                                                  withObject:kernelCode
+                                                  withObject:messageLog];
+        if (messageLog.count > 0) {
+            NSLog(@"Error: %@", messageLog.description);
+        }
+#else
+        NSArray *kernels = [CIKernel kernelsWithString:kernelCode];
+#endif
+        customKernel = kernels.firstObject;
+    }
+    
+    return self;
+}
+
+- (CIImage *)outputImage {
+    CGRect dod = self.imageA.extent;
+    NSArray *arguments = @[self.imageA, self.imageB, @(self.tolerance / 255.0)];
+    CIKernelROICallback roiCallback = ^CGRect(int index, CGRect destRect) {
+        return destRect;
+    };
+    
+    return [customKernel applyWithExtent:dod
+                             roiCallback:roiCallback
+                               arguments:arguments];
+}
+
+@end
