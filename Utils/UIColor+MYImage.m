5595d7373c2e4c5d96e5f448e105f1dc7e672ac3
punmy <panmingyang2009@live.cn>
2020-12-20 15:40:23 +0800

    【Feature】Code arrangement

--- Utils/UIColor+MYImage.m
+++ Utils/UIColor+MYImage.m
@@ -10,9 +10,38 @@
 @implementation UIColor (MYImage)
 
 - (BOOL)compareWithColor:(UIColor *)otherColor {
-    if (YES) {
-        
+    if (!otherColor) {
+        return NO;
     }
+    
+    CGFloat currentAlpha = 0;
+    CGFloat currentBlue = 0;
+    CGFloat currentGreen = 0;
+    CGFloat currentRed = 0;
+    
+    CGFloat othersAlpha = 0;
+    CGFloat othersBlue = 0;
+    CGFloat othersGreen = 0;
+    CGFloat othersRed = 0;
+    
+    [self getRed:&currentRed
+           green:&currentGreen
+            blue:&currentBlue
+           alpha:&currentAlpha];
+           
+    [otherColor getRed:&othersRed
+                 green:&othersGreen
+                  blue:&othersBlue
+                 alpha:&othersAlpha];
+    
+    if (currentRed != othersRed ||
+        currentGreen != othersGreen ||
+        currentBlue != othersBlue ||
+        currentAlpha != othersAlpha) {
+        return NO;
+    }
+    
+    return YES;
 }
 
 @end
