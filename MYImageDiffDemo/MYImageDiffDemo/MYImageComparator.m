44656c70484c74ec36051c17a4d24d5e67208af0
punmy <panmingyang2009@live.cn>
2020-12-20 15:40:37 +0800

    【Feature】Code arrangement

--- MYImageDiffDemo/MYImageDiffDemo/MYImageComparator.m
+++ MYImageDiffDemo/MYImageDiffDemo/MYImageComparator.m
@@ -21,44 +21,6 @@
 #import "MYImageComparator.h"
 #import <CoreImage/CoreImage.h>
 
-@implementation UIColor (MYImage)
-
-- (BOOL)compareWithColor:(UIColor *)otherColor {
-    if (!otherColor) {
-        return NO;
-    }
-    
-    CGFloat currentAlpha = 0;
-    CGFloat currentBlue = 0;
-    CGFloat currentGreen = 0;
-    CGFloat currentRed = 0;
-    
-    CGFloat othersAlpha = 0;
-    CGFloat othersBlue = 0;
-    CGFloat othersGreen = 0;
-    CGFloat othersRed = 0;
-    
-    [self getRed:&currentRed
-           green:&currentGreen
-            blue:&currentBlue
-           alpha:&currentAlpha];
-           
-    [otherColor getRed:&othersRed
-                 green:&othersGreen
-                  blue:&othersBlue
-                 alpha:&othersAlpha];
-    
-    if (currentRed != othersRed ||
-        currentGreen != othersGreen ||
-        currentBlue != othersBlue ||
-        currentAlpha != othersAlpha) {
-        return NO;
-    }
-    
-    return YES;
-}
-
-@end
 
 @interface MYImageComparator ()
 
